de3b8dfeaa0675dd15f9370393ba0aa2013bdb06
pakjkwan <pakjkwan@gmail.com>
2020-10-26 15:10:08 +0900

    15

--- run.py
+++ run.py
@@ -1,2 +1,19 @@
+'''
 from main import app
 app.run(host='127.0.0.1', port='8080', debug=True)
+
+'''
+from flask import Flask
+from flask_restful import Resource, Api
+ 
+app = Flask(__name__)
+api = Api(app)
+ 
+class Rest(Resource):
+    def get(self):
+        return {'rest': 'Good !'}
+ 
+api.add_resource(Rest, '/')
+ 
+if __name__ == '__main__':
+    app.run(debug=True, host='127.0.0.1', port=8080)
